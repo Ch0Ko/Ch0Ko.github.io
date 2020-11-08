2129cd363b0e4479b9f0828af98d5a295df8e1d9
Kaelo <Kaelookoro@gmail.com>
2020-11-08 09:49:44 +0200

    Assignment

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,6 @@
+var d = new Date();
 const $like = document.querySelector("#like");
 $like.addEventListener("click",() => ($like.style.color = "red"));
+document.getElementById("demo").innerHTML = d;
+
+
